5ca3f468d48c57b62a08843f3f24aa0517cbec0b
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-30 16:18:47 -0400

    update

--- conf/settings.py
+++ conf/settings.py
@@ -140,5 +140,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/react-restaurant-app')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/react-restaurant-app/build/static'),)
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/static/build/static'),)
